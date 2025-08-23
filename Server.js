64567e7f89eeb34d3453d051d46da7a9e0880fce
Tejas Agrawal <tejasagrawal36@gmail.com>
2025-08-23 11:39:25 +0530

    Add Login Models

--- Server.js
+++ Server.js
@@ -6,6 +6,7 @@ const port = 3000
 
 const connection = require("./Config/db")
 const RegisterModel = require("./Models/register.models")
+const LoginModel = require("./Models/login.model")
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
@@ -15,6 +16,13 @@ app.set("view engine", "ejs")
 app.get('/', (req, res) => {
   res.render('register')
 })
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
