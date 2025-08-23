3621cb810235ad6b979648724c548bb052521d2b
Tejas Agrawal <tejasagrawal36@gmail.com>
2025-08-23 18:13:41 +0530

    Add Multer and setup comple for img, pdf upload

--- Server.js
+++ Server.js
@@ -4,13 +4,19 @@ const bcrypt = require("bcrypt")
 const dotenv = require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken');
+const multer = require("multer")
 const app = express()
 const port = 3000
 
-app.use(cookieParser())
 
+
+
+app.use(cookieParser())
 const connection = require("./Config/db")
 const RegisterModel = require("./Models/register.models")
+
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -18,7 +24,10 @@ app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
 const mainAUTHroute = require("./Routes/mainAUTH.routes")
+const driveRoute = require("./Routes/drive.routes")
+
 app.use("/", mainAUTHroute)
+app.use(driveRoute)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
